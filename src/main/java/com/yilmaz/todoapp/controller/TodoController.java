f9ec5ad0f2cb87b5eb727f31611048f5950c57db
Serkan <yilmzsrkn@gmail.com>
2024-05-17 09:05:41 +0300

    Minor changes.

--- src/main/java/com/yilmaz/todoapp/controller/TodoController.java
+++ src/main/java/com/yilmaz/todoapp/controller/TodoController.java
@@ -2,12 +2,16 @@ package com.yilmaz.todoapp.controller;
 
 import com.yilmaz.todoapp.dto.todo.TodoDTO;
 import com.yilmaz.todoapp.service.todo.TodoService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,8 +22,16 @@ public class TodoController {
     private final TodoService todoService;
 
     @GetMapping
-    public ResponseEntity<?> getJwtToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
-        return new ResponseEntity<>(authHeader.substring(7), HttpStatus.OK);
+    public ResponseEntity<?> getJwtToken(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+            HttpServletResponse response
+    ) throws IOException, JSONException {
+        response.getWriter().write(
+                new JSONObject().
+                        put("token", authHeader.substring(7)).
+                        toString()
+        );
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("/create-todo")
